550de353b32edbaaac3f6ff30cf7325b2c3cf4c2
Apple MacBook Pro <Apple@Apples-MacBook-Pro-2.local>
2020-02-25 10:39:07 +0600

    AdaptiveWeathe

---
+++ AdaptiveWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveWeather
+//
+//  Created by Apple MacBook Pro on 1/29/20.
+//  Copyright © 2020 Apple MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
